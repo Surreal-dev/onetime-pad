5e18f3a65353ae62091b694cb6508667e9dc87f2
daniel <78708114+Surreal-dev@users.noreply.github.com>
2024-04-15 12:21:35 +0000

    Added code

--- onetime-pad.py
+++ onetime-pad.py
@@ -1,1 +1,38 @@
+import random
 
+plaintext = "This is an ENCRYPTED message!"
+
+def cleantext(message):
+
+    clean = ""
+
+    for letter in message:
+        if letter.isalpha():
+            clean = clean + letter
+
+    return clean
+
+def generatekey(message):
+
+    key = ""
+
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+
+    text = cleantext(message)
+
+    for letter in range(0, len(text)):
+        key = key + random.choice(alphabet)
+
+    return key
+
+key = generatekey(plaintext)
+
+print(key)
+
+def encrypt(message, key):
+
+    encrypted = ""
+
+    for character, letter in message, key:
+
+        encrypted = encrypted + char(ord(character) + ord(letter))
